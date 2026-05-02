e8e19f6c5749b980d5100fbca19782bd3b3d94d4
Arturo Fernandez <bsnux@users.noreply.github.com>
2026-05-01 21:47:56 -0700

    feat: adding auto-completion and LSP conf

--- init.lua
+++ init.lua
@@ -224,119 +224,7 @@ require("lazy").setup({
       })
     end
   },
-  {
-    'hrsh7th/nvim-cmp',
-    dependencies = { 'hrsh7th/vim-vsnip', 'hrsh7th/vim-vsnip-integ', 'hrsh7th/cmp-vsnip' },
-    config = function ()
-      local cmp = require('cmp')
-      cmp.setup {
-        snippet = {
-            expand = function(args)
-              vim.fn["vsnip#anonymous"](args.body)
-            end,
-        },
-        sources = {
-          { name = 'buffer' },
-          { name = 'nvim_lsp' },
-          { name = 'vsnip' },
-        },
-        mapping = cmp.mapping.preset.insert({
-          ['<C-Space>'] = cmp.mapping.complete(),
-          ['<CR>'] = cmp.mapping.confirm {
-            behavior = cmp.ConfirmBehavior.Replace,
-            select = true,
-          },
-          ['<Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            elseif vim.fn['vsnip#expandable']() == 1 then
-              vim.fn['vsnip#expand']()
-            elseif vim.fn['vsnip#jumpable'](1) == 1 then
-              vim.fn['vsnip#jump'](1)
-            else
-              fallback()
-            end
-          end, { 'i', 's' }),
-          -- ['<Tab>'] = cmp.mapping(function(fallback)
-          --   if cmp.visible() then
-          --     cmp.select_next_item()
-          --   elseif luasnip.expand_or_jumpable() then
-          --     luasnip.expand_or_jump()
-          --   else
-          --     fallback()
-          --   end
-          -- end, { 'i', 's' }),
-        }),
-      }
-    end
-  },
-  {
-    'neovim/nvim-lspconfig',
-    dependencies = { 'hrsh7th/cmp-nvim-lsp' },
-    keys = {
-      {'<leader>gg', "<cmd>lua vim.lsp.buf.definition()<CR>", {noremap = true, silent = true}},
-    },
-    opts = {
-     diagnostics = {
-        underline = true,
-        virtual_lines = true,
-        update_in_insert = false,
-        severity_sort = true,
-        float = {
-          border = "rounded",
-          source = "true",
-        },
-        --update_in_insert = false,
-        --virtual_text = {
-        --  spacing = 2,
-        --  source = "if_many",
-        --  prefix = "●",
-        --},
-    }
-   },
-    config = function()
-      local capabilities = require('cmp_nvim_lsp').default_capabilities()
-      require'lspconfig'.pyright.setup{
-        capabilities = capabilities,
-      }
-      require'lspconfig'.jsonls.setup {
-        capabilities = capabilities,
-      }
-      require('lspconfig').yamlls.setup {
-        capabilities = capabilities,
-      }
-      require('lspconfig').terraformls.setup {
-        capabilities = capabilities,
-      }
-      require('lspconfig').powershell_es.setup {
-        capabilities = capabilities,
-        bundle_path = "~/.local/share/pwsh-editor-services",
-      }
-      require('lspconfig').elixirls.setup {
-        capabilities = capabilities,
-        cmd = {"/opt/homebrew/bin/elixir-ls"},
-      }
-      require('lspconfig').ts_ls.setup {
-        capabilities = capabilities,
-        filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
-        --cmd = { "typescript-language-server", "--stdio" },
-      }
-      require('lspconfig').rust_analyzer.setup({
-        capabilities = capabilities,
-        settings = {
-            ['rust-analyzer'] = {
-                check = {
-                    command = "clippy",
-                },
-                checkOnSave = {
-                    enable = true,
-	        },
-	    },
-       }})
-      vim.lsp.config(capabilities)
-      --vim.lsp.enable('gleam')
-    end
-  },
+
   {
     -- `pyright` can't format files so using `black` external plugin
     'psf/black'
@@ -492,16 +380,108 @@ require("lazy").setup({
       })
       vim.keymap.set("n", "<leader>a", "<cmd>AerialToggle!<CR>")
     end,
-  }
-  --{
-  --  "nvim-tree/nvim-tree.lua",
-  --  version = "*",
-  --  lazy = false,
-  --  dependencies = {
-  --    "nvim-tree/nvim-web-devicons",
-  --  },
-  --  config = function()
-  --    require("nvim-tree").setup {}
-  --  end
-  --}
-})
+  },
+  {
+  'saghen/blink.cmp',
+  -- optional: provides snippets for the snippet source
+  dependencies = { 'rafamadriz/friendly-snippets' },
+
+  -- use a release tag to download pre-built binaries
+  version = '1.9.1',
+
+  ---@module 'blink.cmp'
+  ---@type blink.cmp.Config
+  opts = {
+    -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+    -- 'super-tab' for mappings similar to vscode (tab to accept)
+    -- 'enter' for enter to accept
+    -- 'none' for no mappings
+    --
+    -- All presets have the following mappings:
+    -- C-space: Open menu or open docs if already open
+    -- C-n/C-p or Up/Down: Select next/previous item
+    -- C-e: Hide menu
+    -- C-k: Toggle signature help (if signature.enabled = true)
+    --
+    -- See :h blink-cmp-config-keymap for defining your own keymap
+    keymap = { preset = 'default' },
+
+    appearance = {
+      -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+      -- Adjusts spacing to ensure icons are aligned
+      nerd_font_variant = 'mono'
+    },
+
+    -- (Default) Only show the documentation popup when manually triggered
+    completion = { documentation = { auto_show = false } },
+
+    -- Default list of enabled providers defined so that you can extend it
+    -- elsewhere in your config, without redefining it, due to `opts_extend`
+    sources = {
+      default = { 'lsp', 'path', 'snippets', 'buffer' },
+    },
+
+    -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+    -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+    -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+    --
+    -- See the fuzzy documentation for more information
+    fuzzy = { implementation = "prefer_rust_with_warning" }
+  },
+  opts_extend = { "sources.default" }
+},
+  {
+    'neovim/nvim-lspconfig',
+    dependencies = { 'saghen/blink.cmp' },
+    config = function()
+      local capabilities = require('blink.cmp').get_lsp_capabilities()
+
+      vim.lsp.config('yamlls', {
+        capabilities= { capabilities },
+      })
+      vim.lsp.enable('yamlls')
+
+      vim.lsp.config('jsonls', {
+        capabilities= { capabilities },
+      })
+      vim.lsp.enable('jsonls')
+
+      vim.lsp.config('terraformls', {
+        capabilities= { capabilities },
+      })
+      vim.lsp.enable('terraformls')
+
+      vim.lsp.config('ts_ls', {
+        capabilities= { capabilities },
+      })
+      vim.lsp.enable('ts_ls')
+
+      vim.lsp.config('docker_language_server', {
+        capabilities= { capabilities },
+        filetypes = { 'dockerfile', 'Dockerfile', 'yaml.docker-compose' },
+      })
+      vim.lsp.enable('docker_language_server')
+
+      vim.lsp.enable('pylsp', {
+        capabilities= { capabilities },
+      })
+    end
+  },
+  {
+  'nanozuki/tabby.nvim',
+  },
+  {
+  "NeogitOrg/neogit",
+    lazy = true,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "esmuellert/codediff.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+    cmd = "Neogit",
+    keys = {
+      { "<leader>gg", "<cmd>Neogit<cr>", desc = "Show Neogit UI" }
+    }
+}
+
+}) -- lazy.setup end
